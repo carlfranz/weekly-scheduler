b76c8f8c79565e3f898a327bf2de9cc5b2f66e61
Carlo Franzelli <cfranzelli@sorint.it>
2021-07-18 12:52:31 +0200

    fixes export

--- src/WeekExport.jsx
+++ src/WeekExport.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export function WeekExport(props) {
   const { weekData } = props;
 
-  const createRow = (c1, c2, c3) => {
-    const row = document.createElement('tr');
-    const column1 = document.createElement('th');
-    const column2 = document.createElement('td');
-    const column3 = document.createElement('td');
+  const createRow = (c1, c2, c3, header = false) => {
+    const row = document.createElement("tr");
+    const column1 = document.createElement("th");
+    const column2 = document.createElement(header ? "th" : "td");
+    const column3 = document.createElement(header ? "th" : "td");
     column1.innerText = c1;
     column2.innerText = c2;
     column3.innerText = c3;
@@ -18,39 +18,47 @@ export function WeekExport(props) {
   };
 
   const createTable = (data) => {
-    const table = document.createElement('table');
-    table.setAttribute('border', 1);
+    const table = document.createElement("table");
+    table.setAttribute("border", 1);
     const rows = [
+      createRow("", "9:00-13:00", "14:00-18:00", true),
       createRow("monday", data.mon1, data.mon2),
-      createRow("tuesday", data.mon1, data.mon2),
-      createRow("wednesday", data.mon1, data.mon2),
-      createRow("thursday", data.mon1, data.mon2),
-      createRow("friday", data.mon1, data.mon2),
+      createRow("tuesday", data.tue1, data.tue2),
+      createRow("wednesday", data.wed1, data.wed2),
+      createRow("thursday", data.thu1, data.thu2),
+      createRow("friday", data.fri1, data.fri2),
     ];
 
-    rows.forEach(r => table.appendChild(r));
+    rows.forEach((r) => table.appendChild(r));
     return table.outerHTML;
   };
 
   const performExport = () => {
-    navigator.permissions.query({ name: "clipboard-write" }).then(result => {
+    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
       if (result.state == "granted" || result.state == "prompt") {
         const htmlTextTable = createTable(weekData);
         const blob = new Blob([htmlTextTable], { type: "text/html" });
         const clipboardItem = new ClipboardItem({ [blob.type]: blob });
-        navigator.clipboard.write([clipboardItem]).then(() => {
-          alert('Export complete');
-        }, (err) => {
-          alert('Export failed '+err);
-        });
+        navigator.clipboard.write([clipboardItem]).then(
+          () => {
+            alert("Export complete");
+          },
+          (err) => {
+            alert("Export failed " + err);
+          }
+        );
       } else {
-        alert('Clipboard access is required to export data');
+        alert("Clipboard access is required to export data");
       }
     });
   };
   return (
     <div>
-      <button type="button" className="btn btn-link btn-link-blue" onClick={performExport}>
+      <button
+        type="button"
+        className="btn btn-link btn-link-blue"
+        onClick={performExport}
+      >
         copy as html table
       </button>
     </div>
